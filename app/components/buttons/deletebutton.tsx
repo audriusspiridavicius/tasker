d9644f25e923ea190b37821fa5319727a1ffc322
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-04-23 19:26:25 +0300

    custom button components

---
+++ app/components/buttons/deletebutton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+export default function DeleteButton(props:any) {
+  return (
+    <>
+    <button {...props} className={`text-red-500 hover:text-white border border-red-700 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 ${props.className && props.className }`}>
+        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+        </svg>
+    </button>
+    </>
+  )
+}
