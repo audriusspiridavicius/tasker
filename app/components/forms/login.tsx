5f1f2fc1d26377f2bf383f125263b4bf656896c4
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-05-28 23:41:16 +0300

    login form validation

--- app/components/forms/login.tsx
+++ app/components/forms/login.tsx
@@ -4,25 +4,58 @@ import TextInput from '../form_elements/textinput'
 import DefaultButton from '../buttons/defaultbutton'
 import useLogin from '@/app/utils/login'
 import type { User } from '@/app/types/user'
+import { useForm } from 'react-hook-form'
+
+
 
 export default function Login() {
     const {data, error, trigger, isMutating} = useLogin()
 
     const [user, setUser] = useState()
   
-  
+    const {register, handleSubmit, formState:{errors}} = useForm({defaultValues:{email:"", password:""}})
+
+    const onSubmit = () => {trigger(user)}
+    const onError = () => {console.log("error occured!!!!")}
+
     return (
     <>
-        <form> 
+        
+        <form onSubmit={handleSubmit(onSubmit,onError)}> 
             <h1 className="text-2xl font-bold mb-5">Login</h1>
-            data={data?.auth_data}<br/>
-            data={data?.status}
-            <br/>
-            error={error}
-            <TextInput  name="email" id="user-email" type="email" className="mb-5" onChange={(e)=> setUser({...user,email:e.target.value})}/>
-            <TextInput  name="password" id="user-password" type="password" className="mb-5" onChange={(e)=> setUser({...user,password:e.target.value})}/>
 
-            <DefaultButton onClick={()=> trigger(user)}>Login</DefaultButton>
+            <TextInput {...register("email", {required:"Email field is required!"})}  name="email" id="user-email" type="email" className="mb-5" onChange={(e)=> setUser({...user,email:e.target.value})}/>
+            {errors.email && 
+                <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                    {errors.email.message}
+                </div>
+            }
+
+            <TextInput {...register("password", {required:`password field is required`, minLength:{value:4, message:"min length of password is 4"}})}  name="password" id="user-password" type="password" className="mb-5" onChange={(e)=> setUser({...user,password:e.target.value})}/>
+            {errors.password &&
+                <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                    {errors.password.message}
+                </div>
+            }
+            
+            {data?.detail && 
+
+            <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <span className="font-medium">Something went wrong!</span> {data?.detail}
+            </div>}
+            
+            
+            {error && 
+
+                <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                    <span className="font-medium">Something went wrong!</span> {error}
+                </div>
+            }
+
+            <DefaultButton  disabled={isMutating} type="submit">
+                
+            {isMutating ? "Logging in...": "Login"}
+            </DefaultButton>
 
         </form>
     </>
