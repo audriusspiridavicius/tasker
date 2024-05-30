5385e31a914f6785ca35bba4a22a1d132c1e30e2
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-05-31 01:29:43 +0300

    useEffect removed. logOut update

--- app/utils/authenticated.ts
+++ app/utils/authenticated.ts
@@ -1,17 +1,10 @@
 
-import { useEffect } from "react";
-
 export function isAuthenticated(){
-    // useEffect(() => {
-    //     const token = localStorage.getItem('token');
-    //   }, []);
     
     return sessionStorage.getItem("access-token") !== null
 }
 
 export const logOut = () => 
     {
-        sessionStorage.removeItem("access-token")
-        sessionStorage.removeItem("refresh-token")
+        sessionStorage.clear()
     }
-// export const isAuthenticated = () => true
