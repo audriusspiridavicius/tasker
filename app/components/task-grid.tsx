2c565d0da3a104417d4d7b8d52eb92bec4bd52a5
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-06-10 11:14:13 +0300

    tasks list/grid refactor

--- app/components/task-grid.tsx
+++ app/components/task-grid.tsx
@@ -1,36 +1,37 @@
 'use client'
 import React from 'react'
-import { TaskType } from '../types/task'
+import { Priority, TaskType } from '../types/task'
 import AuthorLabel from './authorlabel'
 import DefaultButton from './buttons/defaultbutton'
 import { CurrentTaskContext } from '../page'
 import { useContext } from 'react'
-import useGetTasks from '../utils/get_tasks'
 import useDeleteTask from '../utils/delete_task'
-import GridSkeleton from './gridskeleton'
-import useGetUsers from '../utils/get_users'
 
 
-export default function TaskGrid({ onTaskClick, onDeleteClick}:any) {
+const default_task_value:TaskType = {priority:Priority.LOW, deadline:new Date(), authors:[], name:"", assigned_to:""} 
+
+export default function TaskGrid({ onTaskClick, tasksData}:any) {
   
-    const {tasks, isError, isLoading} = useGetTasks()
+    
+
     const {trigger:trigger_task_delete,isError:deletion_error} = useDeleteTask()
- 
 
-
-    const setTask = useContext(CurrentTaskContext) as any
+    const {currentTask,setCurrentTask} = useContext(CurrentTaskContext)
     
+    const tasks = tasksData?.result?.data
+
+
     // {if (!isAuthenticated) return `not authenticated`}
-    {if (isError) return `An error has occurred. ${isError.message}. Please try again`;}
+    // {if (isError) return `An error has occurred. ${isError.message}. Please try again`;}
     
-    if (isLoading) return <GridSkeleton/>;
+    // if (tasksData?.isMutating) return <GridSkeleton/>;
     // if (tasks.detail) return  tasks.detail;
     return (
     <>
   
     {deletion_error && " error occured while trying to delete task. please try again"}
     <div className='grid-cols-3 grid grid-flow-row gap-2 mb-5 '>
-        {tasks?.length > 0 && tasks && !isError && tasks.map((task:TaskType)=>
+        {tasks?.length > 0 && tasks && tasks.map((task:TaskType)=>
             <div key={task.id} className={`p-2 bg-gray-100 rounded-xl ${task.priority} grid`}>
                 <div className=' text-xl border-b-4 my-2 leading-loose align-middle  uppercase overflow-ellipsis h-12 overflow-hidden text-nowrap '>{task.name}</div>
                 <div className=' uppercase my-2'>{task.priority}</div>
@@ -44,14 +45,13 @@ export default function TaskGrid({ onTaskClick, onDeleteClick}:any) {
                         ): <AuthorLabel className="">none</AuthorLabel>}
                 </div>
                 <div className='flex align-bottom justify-evenly self-end mb-auto content-end place-content-end'>
-                    <DefaultButton className="w-1/2" onClick={()=>{setTask(task);onTaskClick(true);}}>View</DefaultButton>
-                    <DefaultButton className="w-1/2 mr-0" onClick={()=>trigger_task_delete(task.id)}>delete</DefaultButton>
+                    <DefaultButton className="w-1/2" onClick={()=>{setCurrentTask(task);onTaskClick(true);}} >View</DefaultButton>
+                    <DefaultButton className="w-1/2 mr-0" onClick={()=>trigger_task_delete(task.id).then(()=> tasksData.getTasks(1))}>delete</DefaultButton>
                 </div>
 
             </div>
         )}
     </div>
-    
     </>
   )
 }
--- app/components/task-list.tsx
+++ app/components/task-list.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import type { TaskType } from '../types/task'
 import Task  from "./task"
 import { useContext } from 'react'
@@ -12,20 +12,19 @@ import { Avatar } from 'flowbite-react'
 
 const images:string[] = ["person9.png","person10.jpg","person8.jpg","profile7.jpg"]
 
-export default function TaskList({onTaskClick, onDeleteClick}: any) {
+export default function TaskList({onTaskClick, tasksData}: any) {
     
-    const {tasks, isError, isLoading} = useGetTasks()
-    const {trigger, isError:err, isMutating, data} = useDeleteTask()
-    const setTask = useContext(CurrentTaskContext) as any
 
-    {if (isError) return "An error has occurred.";}
-    if (isLoading) return "Loading...";
-    
+    const {trigger, isError:err, data} = useDeleteTask()
+    const tasks = tasksData?.result?.data
+    const {currentTask,setCurrentTask} = useContext(CurrentTaskContext)
+
+    {if (tasksData.isError) return "An error has occurred.";}
+    // if (isLoading) return "Loading...";
+
+
     return (
     <>
-        {err && `ivyko erroras!!!!!!!!!!!!! nepavyko istrinti ${err}`}
-        {isMutating && 'isMutating!!!!!!!!!!!!! vyksta'}
-        {data && data.success && "pavyko istrinti sekmingai"}
        <div className='w-11/12 flex mb-4'>
         <div className='w-1/6 capitalize text-center'></div>
         <div className='w-3/6 capitalize'>task Name</div>
@@ -35,7 +34,7 @@ export default function TaskList({onTaskClick, onDeleteClick}: any) {
 
         {tasks?.map((task:TaskType)=>
             <div key={task.id} className='flex'>
-                <div onClick={()=>{setTask(task); onTaskClick(true)} }  className='w-11/12 flex py-5 my-2 border border-gray-200 rounded-lg shadow bg-white hover:shadow-inner hover:cursor-pointer hover:bg-gray-50'>
+                <div onClick={()=>{setCurrentTask(task); onTaskClick(true)} }  className='w-11/12 flex py-5 my-2 border border-gray-200 rounded-lg shadow bg-white hover:shadow-inner hover:cursor-pointer hover:bg-gray-50'>
                     <div className='w-1/6 capitalize text-center'>
                     <Avatar.Group>
                         {images.map((image, index)=>
@@ -51,7 +50,7 @@ export default function TaskList({onTaskClick, onDeleteClick}: any) {
                 </div>
                 <div className='w-1/12 items-center flex justify-center'>
                     
-                    <DeleteButton className="ml-10 mb-0" onClick={()=>trigger(task.id)}/>
+                    <DeleteButton className="ml-10 mb-0" onClick={()=>trigger(task.id).then(()=> tasksData.getTasks(1))}/>
                 </div>
             </div>
             
