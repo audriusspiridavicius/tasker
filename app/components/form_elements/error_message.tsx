ac9846c0ea90424cfb66cf7092472db744191968
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-06-12 13:58:53 +0300

    profile page edit form error message

---
+++ app/components/form_elements/error_message.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ErrorMessage({children}) {
+  
+if (!children) return ""  
+return (
+    <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+        {children}
+    </div>
+  )
+}
